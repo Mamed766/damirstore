278d026fe3cf774f674e651a611d23b948b73506
Mamed766 <mehemmeddemirli57@gmail.com>
2025-01-01 19:45:19 +0400

    chore:menu created for responsive

--- components/shared/header/index.tsx
+++ components/shared/header/index.tsx
@@ -1,10 +1,7 @@
-import { Button } from "@/components/ui/button";
 import { APP_NAME } from "@/lib/constants";
-import { ShoppingCart, UserIcon } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
-import ModeToggle from "./mode-toggle";
-
+import Menu from "./menu";
 const Header = () => {
   return (
     <header className="w-full border-b">
@@ -23,19 +20,7 @@ const Header = () => {
             </span>
           </Link>
         </div>
-        <div className="space-x-2">
-          <ModeToggle />
-          <Button asChild variant="ghost">
-            <Link href="/cart">
-              <ShoppingCart /> Cart
-            </Link>
-          </Button>
-          <Button asChild variant="ghost">
-            <Link href="/sign-in">
-              <UserIcon /> Sign In
-            </Link>
-          </Button>
-        </div>
+        <Menu />
       </div>
     </header>
   );
