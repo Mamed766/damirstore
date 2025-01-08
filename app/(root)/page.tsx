ae6cad7e2dab814077027309519e21e5b1154cd3
Mamed766 <mehemmeddemirli57@gmail.com>
2025-01-08 20:05:41 +0400

    Chore: Paymet Method FOrm & Update

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,6 +1,5 @@
 import ProductList from "@/components/shared/product/product-list";
 import { getLatestProducts } from "@/lib/actions/product.actions";
-
 const Home = async () => {
   const latestProducts = await getLatestProducts();
 
