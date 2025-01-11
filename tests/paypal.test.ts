4aeb9c5ca29f66166921680904b4b7c466d14437
Mamed766 <mehemmeddemirli57@gmail.com>
2025-01-11 15:42:04 +0400

    chore: Jest testing

---
+++ tests/paypal.test.ts
@@ -0,0 +1,11 @@
+import { generateAccessToken } from "../lib/paypal";
+
+//Test to generate acces token from paypal
+
+test("generates token from paypal", async () => {
+  const tokenResponse = await generateAccessToken();
+
+  console.log(tokenResponse);
+  expect(typeof tokenResponse).toBe("string");
+  expect(tokenResponse.length).toBeGreaterThan(0);
+});
