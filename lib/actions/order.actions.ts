7501b28a3daa590b0cfb9a0164c364653b029d58
Mamed766 <mehemmeddemirli57@gmail.com>
2025-01-12 19:09:19 +0400

    chore: Create Paypal order action

--- lib/actions/order.actions.ts
+++ lib/actions/order.actions.ts
@@ -6,8 +6,10 @@ import { getMyCart } from "./cart.action";
 import { getUserById } from "./user.actions";
 import { insertOrderSchema } from "../validators";
 import { prisma } from "@/db/prisma";
-import { CartItem } from "@/types";
+import { CartItem, PaymentResult } from "@/types";
 import { auth } from "@/auth";
+import { paypal } from "../paypal";
+import { revalidatePath } from "next/cache";
 
 //Create order and create the order items
 
@@ -117,4 +119,46 @@ export async function getOrderById(orderId: string) {
   });
 
   return convertToPlainObject(data);
+}
+
+//Create new paypal order
+export async function createPayPalOrder(orderId: string) {
+  try {
+    //Get order from database
+    const order = await prisma.order.findFirst({
+      where: {
+        id: orderId,
+      },
+    });
+
+    if (order) {
+      // Create paypal order
+      const paypalOrder = await paypal.createOrder(Number(order.totalPirce));
+
+      //Update order with paypal order id
+      await prisma.order.update({
+        where: {
+          id: orderId,
+        },
+        data: {
+          paymentResult: {
+            id: paypalOrder.id,
+            email_address: "",
+            status: "",
+            pricePaid: 0,
+          },
+        },
+      });
+
+      return {
+        success: true,
+        message: "Item order created succesfully",
+        data: paypalOrder.id,
+      };
+    } else {
+      throw new Error("Order not found");
+    }
+  } catch (error) {
+    return { succes: false, message: formatError(error) };
+  }
 }
