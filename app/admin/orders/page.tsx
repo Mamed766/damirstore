ac36e923a5d60871f6630b00681b978be009eb84
Mamed766 <mehemmeddemirli57@gmail.com>
2025-01-26 18:41:27 +0400

    chore:user search

--- app/admin/orders/page.tsx
+++ app/admin/orders/page.tsx
@@ -20,9 +20,9 @@ export const metadata: Metadata = {
 };
 
 const AdminOrdersPage = async (props: {
-  searchParams: Promise<{ page: string }>;
+  searchParams: Promise<{ page: string; query: string }>;
 }) => {
-  const { page = "1" } = await props.searchParams;
+  const { page = "1", query: searchText } = await props.searchParams;
 
   const session = await auth();
 
@@ -31,18 +31,32 @@ const AdminOrdersPage = async (props: {
 
   const orders = await getAllOrders({
     page: Number(page),
+    query: searchText,
   });
 
   return (
     <div className="space-y-2">
-      <div className="h2-bold">Orders</div>
+      <div className="flex items-center gap-3">
+        <h1 className="h2-bold">Orders</h1>
+        {searchText && (
+          <div>
+            Filtered by <i>&quot;{searchText}&quot;</i>{" "}
+            <Link href="/admin/orders">
+              <Button variant="outline" size="sm">
+                Remove Filter
+              </Button>
+            </Link>
+          </div>
+        )}
+      </div>
 
       <div className="overflow-x-auto">
         <Table>
           <TableHeader>
             <TableRow>
               <TableHead>ID</TableHead>
-              <TableHead>DATA</TableHead>
+              <TableHead>DATE</TableHead>
+              <TableHead>BUYER</TableHead>
               <TableHead>TOTAL</TableHead>
               <TableHead>PAID</TableHead>
               <TableHead>DELIVERED</TableHead>
@@ -57,6 +71,7 @@ const AdminOrdersPage = async (props: {
                   <TableCell>
                     {formatDateTime(order.createdAt).dateTime}
                   </TableCell>
+                  <TableCell>{order.user.name}</TableCell>
                   <TableCell>{formatCurrency(order.totalPirce)}</TableCell>
                   <TableCell>
                     {order.isPaid && order.paidAt
@@ -72,7 +87,7 @@ const AdminOrdersPage = async (props: {
                     <Button asChild variant="outline" size="sm">
                       <Link href={`/order/${order.id}`}>Details</Link>
                     </Button>
-                    {/* @ts-expect-error action*/}
+                    {/* @ts-expect-error action */}
                     <DeleteDialog id={order.id} action={deleteOrder} />
                   </TableCell>
                 </TableRow>
--- components/admin/admin-search.tsx
+++ components/admin/admin-search.tsx
@@ -1,42 +1,39 @@
 "use client";
 
-import { useRouter, usePathname, useSearchParams } from "next/navigation";
 import { useState, useEffect } from "react";
+import { usePathname, useSearchParams } from "next/navigation";
 import { Input } from "../ui/input";
 
-export default function AdminSearch() {
-  const router = useRouter();
+const AdminSearch = () => {
   const pathname = usePathname();
-  const searchParams = useSearchParams();
-
   const formActionUrl = pathname.includes("/admin/orders")
     ? "/admin/orders"
     : pathname.includes("/admin/users")
     ? "/admin/users"
     : "/admin/products";
 
+  const searchParams = useSearchParams();
   const [queryValue, setQueryValue] = useState(searchParams.get("query") || "");
 
   useEffect(() => {
     setQueryValue(searchParams.get("query") || "");
   }, [searchParams]);
 
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const newValue = e.target.value;
-    setQueryValue(newValue);
-
-    router.replace(`${formActionUrl}?query=${encodeURIComponent(newValue)}`);
-  };
-
   return (
-    <div>
+    <form action={formActionUrl} method="GET">
       <Input
         type="search"
         placeholder="Search..."
         name="query"
         value={queryValue}
-        onChange={handleChange}
+        onChange={(e) => setQueryValue(e.target.value)}
+        className="md:w-[100px] lg:w-[300px]"
       />
-    </div>
+      <button className="sr-only" type="submit">
+        Search
+      </button>
+    </form>
   );
-}
+};
+
+export default AdminSearch;
